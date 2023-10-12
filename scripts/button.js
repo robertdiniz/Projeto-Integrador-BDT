d0d2ae8a78f0310c676a5010924a94bb2f0d26b6
Robert <dinizrobert2002@gmail.com>
2023-10-12 17:57:51 -0300

    static files

---
+++ scripts/button.js
@@ -0,0 +1,7 @@
+botao_menu = document.querySelector('.menu-burger');
+pages = document.querySelector('.pages');
+sessao = document.querySelector('.sessao');
+
+function toggleMenu(){
+    sessao.classList.toggle("sessao-active");
+};

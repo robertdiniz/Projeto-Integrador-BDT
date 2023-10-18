c7e3bb471ee83021dd55c47c09a399a054297166
Rian <rlina1785@gmail.com>
2023-10-18 16:40:52 -0300

    componentes da trilha

---
+++ scripts/enviarRepositorio.js
@@ -0,0 +1,7 @@
+fade = document.querySelector('.fade');
+modal = document.querySelector('.modal');
+
+function enviarRepositorio(){
+    fade.classList.toggle('modal-active');
+    modal.classList.toggle('modal-active');
+}
